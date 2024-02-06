6ffd8adadf4a4644caa009a116d2a95959fe3cd0
Harada-Reina <harada-r@pca.co.jp>
2024-02-06 15:53:09 +0900

    feat:init-knex

---
+++ db/migrations/20240206061343_create_product_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("product", function (table){
+    table.increments("id").primary().unique().notNullable();
+    table.string("name", 32).notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("product");
+};
--- script/create/create-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE product (id serial unique, name varchar(50) unique);
-
-CREATE TABLE maxid(
-    product_name varchar(50) unique,
-    id serial
-);
