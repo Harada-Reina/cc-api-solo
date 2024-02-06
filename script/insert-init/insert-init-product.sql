6dc8fcf9f8b3352a6a6f28f0ac2ffa18e7402eea
Harada-Reina <harada-r@pca.co.jp>
2024-02-06 14:34:14 +0900

    feat:create-database

---
+++ script/insert-init/insert-init-product.sql
@@ -0,0 +1,20 @@
+-- product
+INSERT INTO product VALUES (1, 'ちいかわ');
+INSERT INTO product VALUES (2, 'ハチワレ');
+INSERT INTO product VALUES (3, 'うさぎ');
+INSERT INTO product VALUES (4, 'モモンガ');
+INSERT INTO product VALUES (5, 'くりまんじゅう');
+INSERT INTO product VALUES (6, 'ラッコ');
+INSERT INTO product VALUES (7, 'シーサー');
+INSERT INTO product VALUES (8, 'キメラ');
+INSERT INTO product VALUES (9, 'ちいかわ(カニ)');
+INSERT INTO product VALUES (10, 'ハチワレ(カニ)');
+INSERT INTO product VALUES (11, 'うさぎ(ロブスター)');
+INSERT INTO product VALUES (12, 'ハチワレとうさぎ');
+INSERT INTO product VALUES (13, 'ハチワレとラッコ');
+INSERT INTO product VALUES (14, 'あのことでかつよ');
+INSERT INTO product VALUES (15, '水風呂');
+INSERT INTO product VALUES (16, '【レア】お布団');
+
+-- maxid
+INSERT INTO maxid VALUES ('product', 16);
