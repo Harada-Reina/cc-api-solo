c9353222596649be40679974ab4a01a4a29924b4
Harada-Reina <harada-r@pca.co.jp>
2024-02-07 18:02:02 +0900

    modify delete()

--- test/product.spec.js
+++ test/product.spec.js
@@ -1,35 +1,35 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 
 chai.use(chaiHttp);
 chai.should();
 
 const expect = chai.expect;
 
-const { setupServer } = require('../src/server');
+const { setupServer } = require("../src/server");
 
-const server = setupServer('8000');
+const server = setupServer("8000");
 
 let request;
 beforeEach(() => {
   request = chai.request(server);
 });
 
-let newProduct = { id: 9999, name: 'ちいかわ(パジャマ)' };
-const newName = 'ハチワレ(パジャマ)';
+let newProduct = { id: 9999, name: "ちいかわ(パジャマ)" };
+const newName = "ハチワレ(パジャマ)";
 let addedCount = 0;
 
-describe('solo project server test', () => {
-  describe('POST /products', () => {
-    it('should added product', async () => {
-      const res = await request.post('/products').send(newProduct);
+describe("solo project server test", () => {
+  describe("POST /products", () => {
+    it("should added product", async () => {
+      const res = await request.post("/products").send(newProduct);
       res.should.have.status(200);
     });
   });
 
-  describe('GET /products', () => {
-    it('should return product all array', async () => {
-      const res = await request.get('/products');
+  describe("GET /products", () => {
+    it("should return product all array", async () => {
+      const res = await request.get("/products");
       res.should.be.json;
       const result = JSON.parse(res.text);
 
@@ -42,8 +42,8 @@ describe('solo project server test', () => {
     });
   });
 
-  describe('PATCH /products/:id', () => {
-    it('should return updated id', async () => {
+  describe("PATCH /products/:id", () => {
+    it("should return updated id", async () => {
       newProduct.name = newName;
 
       const res = await request
@@ -57,30 +57,29 @@ describe('solo project server test', () => {
     });
   });
 
-  describe('GET /products/:id', () => {
-    it('should return product by id', async () => {
+  describe("GET /products/:id", () => {
+    it("should return product by id", async () => {
       const res = await request.get(`/products/${newProduct.id}`);
       res.should.be.json;
       const result = JSON.parse(res.text);
 
-      expect(typeof result).to.equal('object');
+      expect(typeof result).to.equal("object");
       expect(result.id).equal(newProduct.id);
       expect(result.name).equal(newName);
     });
   });
 
-  describe('DELETE /products/:id', () => {
-    it('should added product', async (done) => {
-      await request.delete(`/products/${newProduct.id}`);
+  describe("DELETE /products/:id", () => {
+    it("should added product", async () => {
+      const res = await request.delete(`/products/${newProduct.id}`);
 
       res.should.have.status(204);
-      done();
     });
   });
 
-  describe('GET /products', () => {
-    it('should return default products', async () => {
-      const res = await request.get('/products');
+  describe("GET /products", () => {
+    it("should return default products", async () => {
+      const res = await request.get("/products");
       res.should.be.json;
       const result = JSON.parse(res.text);
       expect(result.length).to.equal(addedCount - 1);
